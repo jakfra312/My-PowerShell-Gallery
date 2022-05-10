ffaefc76e1ba3bc9bb771ded38f52016d982ff00
Jakub Frąckowiak <78365197+ZedeKage@users.noreply.github.com>
2022-05-10 16:00:22 +0200

    WUpdate
    Automatically installs windows updates. Works well with windows task scheduler

---
+++ Miniscripts/Script 1 Windows Update/WUpdate.ps1
@@ -0,0 +1,12 @@
+if (Get-Module -ListAvailable -Name PSWindowsUpdate){
+   Import-Module PSWindowsUpdate -Force
+   if ((Get-WindowsUpdate).Title -eq "") {
+        exit   
+   }
+   else {
+    Install-WindowsUpdate -AcceptAll -AutoReboot
+   }
+} else {
+    Install-Module -Name PSWindowsUpdate -Force
+}
+#Author of script: Jakub Frąckowiak
