a4d3cb58f0ad02ed2188246e195aeaa156148f91
Jakub Frąckowiak <78365197+ZedeKage@users.noreply.github.com>
2022-05-21 15:57:41 +0200

    Expiry Notification
    This script checks whether the Active Directory module is installed and whether the given user has an e-mail assigned, if not, it will inform about it in the console. Then it checks how many days are left until the password expires. If less than 20, it will send a notification to the e-mail assigned in Active Directory.

---
+++ Script 1 Expiry Notification/Expiry Notification.ps1
@@ -0,0 +1,35 @@
+﻿#Author: Jakub Frąckowiak
+#This script requires Active Directory Module
+if(Get-Module -ListAvailable -Name ActiveDirectory){
+$ADUsers = Get-ADUser -Filter * -Properties * | Where-Object {($_.Enabled -eq $true) -and ($_.PasswordNeverExpires -eq $false) -and -not ($_.accountExpires -eq 0)}
+$MaxDays = (Get-ADDefaultDomainPasswordPolicy).MaxPasswordAge
+$Serv = (Get-ADDomainController).Forest
+$Info = $ADUsers | select Name, mail, SamAccountName, PasswordLastSet 
+Foreach($User in $Info){
+if(-not $User.mail){
+   Write-Host "User: $($User.Name) doesn't have an assigned E-mail address"
+} else {
+    $LastSet = $User.PasswordLastSet
+    $PP = Get-ADUserResultantPasswordPolicy -Identity $User.SamAccountName -Server $Serv 
+    if($PP){
+        $MaxDays = $PP.maxPasswordAge}
+    else{    
+        $ExpDate = $LastSet + $MaxDays
+        $Today = [datetime]::Today
+        $ExpDays = (New-TimeSpan -Start $Today -End $ExpDate).Days
+        if($ExpDays -lt 20){
+            $Msg = "Your password will expire in $ExpDays days"
+            $SMTPServer = 'smtp.gmail.com'
+            $SMTPPort = '587'
+            $From = 'testpass1@gmail.com'
+            $To = $User.mail
+            $Pass = Get-Content C:\Test\passexample.txt
+            $Pass = ConvertTo-SecureString -String $Pass -Force -AsPlainText 
+            $creden = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $From, $Pass
+            Send-MailMessage -From $From -To $To -Subject 'Password Notification' -Body $Msg -SmtpServer $SMTPServer -Port $SMTPPort -Credential $creden -UseSsl
+        }
+    
+}}}}
+else{
+Write-Host 'No ActiveDirectory Module'
+}
