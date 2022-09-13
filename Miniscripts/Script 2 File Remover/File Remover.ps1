72636a6b17bdd626c3a3b55089f4abc9da0eae00
Jakub Frąckowiak <78365197+jakfra312@users.noreply.github.com>
2022-09-13 17:23:05 +0200

    SCH
    SCH

--- Miniscripts/Script 2 File Remover/File Remover.ps1
+++
@@ -1,5 +0,0 @@
-#Author of script: Jakub Frąckowiak
-$P = 'C:\Users\User01\Downloads\'
-$Today = Get-Date
-$14Ago = $Today.AddDays(-14)
-Get-ChildItem -Path $P -Recurse | Where-Object {($_.LastWriteTime -lt $14Ago)} | Remove-Item
